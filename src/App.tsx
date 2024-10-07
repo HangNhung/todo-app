bb3873f87f64e6e4d98c6a79614802db013649d9
hang nhung <hangbichnhung@gmail.com>
2024-10-08 01:43:40 +0700

    fix: run port 4000

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import TaskList from './components/TaskList';
 import Filter from './components/Filter';
 import { useEffect, useState } from 'react';
 
-const API_URL = 'http://localhost:5173/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Mock API
 const mockAPI = {
